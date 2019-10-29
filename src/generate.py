6d54974b479631d07af8417b9e4086b01cc3ca44
zhangxiaosa <15927119728@163.com>
2019-10-28 23:34:52 -0500

    modify generate.py

--- src/generate.py
+++ src/generate.py
@@ -11,34 +11,34 @@ if not os.path.exists(path):
 
 # -0
 for i in range(13, 100):
-	subprocess.run(["fuzz", "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+	subprocess.call(["fuzz", "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 
 start = 100
 
 
 # -a
 for i in range(start, start+100):
-	subprocess.run(["fuzz", "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+	subprocess.call(["fuzz", "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -p
 for i in range(start, start+100):
-	subprocess.run(["fuzz", "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+	subprocess.call(["fuzz", "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -0 + -l
 for i in range(start, start+100):
 	l = random.randint(1, 255)
-	subprocess.run(["fuzz", "-l %d" % l, "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+	subprocess.call(["fuzz", "-l %d" % l, "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -a + -l
 for i in range(start, start+100):
 	l = random.randint(1, 255)
-	subprocess.run(["fuzz", "-l %d" % l, "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+	subprocess.call(["fuzz", "-l %d" % l, "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -p + -l
 for i in range(start, start+100):
 	l = random.randint(1, 255)
-	subprocess.run(["fuzz", "-l %d" % l, "-p", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+	subprocess.call(["fuzz", "-l %d" % l, "-p", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
