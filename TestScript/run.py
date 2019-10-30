8ddb1e18e2d8f21a22c3f2f1ab45c87c9dc1531f
zhangxiaosa <15927119728@163.com>
2019-10-29 23:02:47 -0500

    indent

--- TestScript/run.py
+++ TestScript/run.py
@@ -19,98 +19,98 @@ output_file = open(output_filename, "w")
 # test cases path
 test_list = []
 for i in range(1, test_num+1):
-	test_list.append("%s%s%d" % (test_path, test_prefix, i))
+  test_list.append("%s%s%d" % (test_path, test_prefix, i))
 
 
 
 def run_file(item, output, test_list, fnull, timeout):
-	type = item.split(" ", 1)[0]
-	cmd = item.split(" ", 1)[1]
-        hang_count = 0
-	for test_case in test_list:
-                if hang_count >= 5:
-                    break
-		try:
-			retcode = subprocess.call(["%s" % cmd, "%s" % test_case], stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
-		except(subprocess.TimeoutExpired):
-                        hang_count = hang_count + 1
-			output.write("%s %s %s hang\n" % (type, cmd, test_case))
-		except(FileNotFoundError):
-			output.write("%s %s not found\n" % (type, cmd))
-			break
-		else:
-                        hang_count = 0
-			if retcode < 0:
-				output.write("%s %s %s error: %d\n" % (type, cmd, test_case, retcode))
-		output.flush()
+  type = item.split(" ", 1)[0]
+  cmd = item.split(" ", 1)[1]
+  hang_count = 0
+  for test_case in test_list:
+    if hang_count >= 5:
+      break
+    try:
+      retcode = subprocess.call(["%s" % cmd, "%s" % test_case], stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
+    except(subprocess.TimeoutExpired):
+      hang_count = hang_count + 1
+      output.write("%s %s %s hang\n" % (type, cmd, test_case))
+    except(FileNotFoundError):
+      output.write("%s %s not found\n" % (type, cmd))
+      break
+    else:
+      hang_count = 0
+      if retcode < 0:
+        output.write("%s %s %s error: %d\n" % (type, cmd, test_case, retcode))
+    output.flush()
 
 def run_cp(item, output, test_list, fnull, timeout):
-	type = item.split(" ", 2)[0]
-	file_tmp = item.split(" ", 2)[1]
-	cmd = item.split(" ", 2)[2]
-        hang_count = 0
-	for test_case in test_list:
-                if hang_count >= 5:
-                    break
-		try:
-			subprocess.call(["cp", "%s" % test_case, "%s" % file_tmp])
-			retcode = subprocess.call(["%s" % cmd, "%s" % file_tmp], stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
-		except(subprocess.TimeoutExpired):
-                        hang_count = hang_count + 1
-			output.write("%s %s %s hang\n" % (type, cmd, test_case))
-		except(FileNotFoundError):
-			output.write("%s %s not found\n" % (type, cmd))
-			break
-		else:
-                        hang_count = 0
-			if retcode < 0:
-				output.write("%s %s %s error: %d\n" % (type, cmd, test_case, retcode))
-		subprocess.call(["rm", "%s" % file_tmp])
-		output.flush()
+  type = item.split(" ", 2)[0]
+  file_tmp = item.split(" ", 2)[1]
+  cmd = item.split(" ", 2)[2]
+  hang_count = 0
+  for test_case in test_list:
+    if hang_count >= 5:
+      break
+    try:
+      subprocess.call(["cp", "%s" % test_case, "%s" % file_tmp])
+      retcode = subprocess.call(["%s" % cmd, "%s" % file_tmp], stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
+    except(subprocess.TimeoutExpired):
+      hang_count = hang_count + 1
+      output.write("%s %s %s hang\n" % (type, cmd, test_case))
+    except(FileNotFoundError):
+      output.write("%s %s not found\n" % (type, cmd))
+      break
+    else:
+      hang_count = 0
+      if retcode < 0:
+        output.write("%s %s %s error: %d\n" % (type, cmd, test_case, retcode))
+        subprocess.call(["rm", "%s" % file_tmp])
+    output.flush()
 	
 def run_stdin(item, output, test_list, fnull, timeout):
-	type = item.split(" ", 1)[0]
-	cmd = item.split(" ", 1)[1]
-        hang_count = 0
-	for test_case in test_list:
-                if hang_count >= 5:
-                    break
-		try:
-			retcode = subprocess.call(["%s" % cmd, "< %s" % test_case], stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
-		except(subprocess.TimeoutExpired):
-                        hang_count = hang_count + 1
-			output.write("%s %s %s hang\n" % (type, cmd, test_case))
-		except(FileNotFoundError):
-			output.write("%s %s not found\n" % (type, cmd))
-			break
-		else:
-                        hang_count = 0
-			if retcode < 0:
-				output.write("%s %s %s error: %d\n" % (type, cmd, test_case, retcode))
-		output.flush()
+  type = item.split(" ", 1)[0]
+  cmd = item.split(" ", 1)[1]
+  hang_count = 0
+  for test_case in test_list:
+    if hang_count >= 5:
+      break
+    try:
+      retcode = subprocess.call(["%s" % cmd, "< %s" % test_case], stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
+    except(subprocess.TimeoutExpired):
+      hang_count = hang_count + 1
+      output.write("%s %s %s hang\n" % (type, cmd, test_case))
+    except(FileNotFoundError):
+      output.write("%s %s not found\n" % (type, cmd))
+      break
+    else:
+      hang_count = 0
+      if retcode < 0:
+        output.write("%s %s %s error: %d\n" % (type, cmd, test_case, retcode))
+    output.flush()
 
 def run_double(item, output, test_list, fnull, timeout):
-	type = item.split(" ", 1)[0]
-	cmd = item.split(" ", 1)[1]
-        hang_count = 0
-	for i in range(len(test_list)):
-                if hang_count >= 5:
-                    break
-		test_case1 = random.choice(test_list)
-		test_case2 = random.choice(test_list)
-		try:
-			retcode = subprocess.call(["%s" % cmd, "%s %s" % (test_case1, test_case2)], stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
-		except(subprocess.TimeoutExpired):
-                        hang_count = hang_count + 1
-			output.write("%s %s %s %s hang\n" % (type, cmd, test_case1, test_case2))
-		except(FileNotFoundError):
-			output.write("%s %s not found\n" % (type, cmd))
-			break
-		else:
-                        hang_count = 0
-			if retcode < 0:
-				output.write("%s %s %s %s error: %d\n" % (type, cmd, test_case1, test_case2, retcode))
-		output.flush()
+  type = item.split(" ", 1)[0]
+  cmd = item.split(" ", 1)[1]
+  hang_count = 0
+  for i in range(len(test_list)):
+    if hang_count >= 5:
+      break
+    test_case1 = random.choice(test_list)
+    test_case2 = random.choice(test_list)
+    try:
+      retcode = subprocess.call(["%s" % cmd, "%s %s" % (test_case1, test_case2)], stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
+    except(subprocess.TimeoutExpired):
+      hang_count = hang_count + 1
+      output.write("%s %s %s %s hang\n" % (type, cmd, test_case1, test_case2))
+    except(FileNotFoundError):
+      output.write("%s %s not found\n" % (type, cmd))
+      break
+    else:
+      hang_count = 0
+      if retcode < 0:
+        output.write("%s %s %s %s error: %d\n" % (type, cmd, test_case1, test_case2, retcode))
+    output.flush()
 
 
 
