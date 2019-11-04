ec2177349bd77ed79a96a6e17bdbb8ef38b39ba0
zhangxiaosa <15927119728@163.com>
2019-11-03 19:11:42 -0600

    generate.py indent

--- src/generate.py
+++ src/generate.py
@@ -6,60 +6,78 @@ fnull = open(os.devnull, 'w')
 path = "./NewTest"
 
 if not os.path.exists(path):
-	os.mkdir(path)
+  os.mkdir(path)
 
 
 # -0
 for i in range(13, 100):
-	subprocess.call(["fuzz", "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+  if os.path.isfile(os.path.join(path, "t%d" % i)):
+  	continue
+  subprocess.call(["fuzz", "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 
 start = 100
 
 
 # -a
 for i in range(start, start+100):
-	subprocess.call(["fuzz", "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+  if os.path.isfile(os.path.join(path, "t%d" % i)):
+  	continue
+  subprocess.call(["fuzz", "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -p
 for i in range(start, start+100):
-	subprocess.call(["fuzz", "-p", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+  if os.path.isfile(os.path.join(path, "t%d" % i)):
+  	continue
+  subprocess.call(["fuzz", "-p", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -0 + -l
 for i in range(start, start+100):
-	l = random.randint(1, 255)
-	subprocess.call(["fuzz", "-l %d" % l, "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+  if os.path.isfile(os.path.join(path, "t%d" % i)):
+  	continue
+  l = random.randint(1, 255)
+  subprocess.call(["fuzz", "-l %d" % l, "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -a + -l
 for i in range(start, start+100):
-	l = random.randint(1, 255)
-	subprocess.call(["fuzz", "-l %d" % l, "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+  if os.path.isfile(os.path.join(path, "t%d" % i)):
+  	continue
+  l = random.randint(1, 255)
+  subprocess.call(["fuzz", "-l %d" % l, "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -p + -l
 for i in range(start, start+100):
-	l = random.randint(1, 255)
-	subprocess.call(["fuzz", "-l %d" % l, "-p", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+  if os.path.isfile(os.path.join(path, "t%d" % i)):
+  	continue
+  l = random.randint(1, 255)
+  subprocess.call(["fuzz", "-l %d" % l, "-p", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -0 + -l + 5000
 for i in range(start, start+100):
-	l = random.randint(1, 255)
+  if os.path.isfile(os.path.join(path, "t%d" % i)):
+  	continue
+  l = random.randint(1, 255)
         n = random.randint(1, 5000)
-	subprocess.call(["fuzz", "%d" % n, "-l %d" % l, "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+  subprocess.call(["fuzz", "%d" % n, "-l %d" % l, "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -a + -l + 5000
 for i in range(start, start+100):
-	l = random.randint(1, 255)
+  if os.path.isfile(os.path.join(path, "t%d" % i)):
+  	continue
+  l = random.randint(1, 255)
         n = random.randint(1, 5000)
-	subprocess.call(["fuzz", "%d" % n, "-l %d" % l, "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+  subprocess.call(["fuzz", "%d" % n, "-l %d" % l, "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
 # -p + -l + 5000
 for i in range(start, start+100):
-	l = random.randint(1, 255)
+  if os.path.isfile(os.path.join(path, "t%d" % i)):
+  	continue
+  l = random.randint(1, 255)
         n = random.randint(1, 5000)
-	subprocess.call(["fuzz", "%d" % n, "-l %d" % l, "-p", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
+  subprocess.call(["fuzz", "%d" % n, "-l %d" % l, "-p", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
