8183924cbee1e81259bca78e582f4c9c5a52e239
zhangxiaosa <15927119728@163.com>
2019-11-03 19:12:56 -0600

    generate.py indent

--- src/generate.py
+++ src/generate.py
@@ -61,7 +61,7 @@ for i in range(start, start+100):
   if os.path.isfile(os.path.join(path, "t%d" % i)):
   	continue
   l = random.randint(1, 255)
-        n = random.randint(1, 5000)
+  n = random.randint(1, 5000)
   subprocess.call(["fuzz", "%d" % n, "-l %d" % l, "-0", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
@@ -70,7 +70,7 @@ for i in range(start, start+100):
   if os.path.isfile(os.path.join(path, "t%d" % i)):
   	continue
   l = random.randint(1, 255)
-        n = random.randint(1, 5000)
+  n = random.randint(1, 5000)
   subprocess.call(["fuzz", "%d" % n, "-l %d" % l, "-a", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
 start = start + 100
 
@@ -79,5 +79,5 @@ for i in range(start, start+100):
   if os.path.isfile(os.path.join(path, "t%d" % i)):
   	continue
   l = random.randint(1, 255)
-        n = random.randint(1, 5000)
+  n = random.randint(1, 5000)
   subprocess.call(["fuzz", "%d" % n, "-l %d" % l, "-p", "-o", os.path.join(path, "t%d" % i)], stdout=fnull, stderr=subprocess.STDOUT)
