107edc29303ce7501948acbb3de2670250146584
zhangxiaosa <15927119728@163.com>
2019-11-14 21:09:37 -0600

    debug

--- TestScript/run.py
+++ TestScript/run.py
@@ -3,23 +3,41 @@ import subprocess
 import sys
 import random
 
-# can change
-test_path = "./NewTest/"
-test_prefix = "t"
-timeout = 20
-arg_error_detect = 3
-output_filename_dir = "./output_log_899"
+############
+# options
+# where are the test cases
+test_dir = "./NewTest/"
 
-# can't change
-fnull = open(os.devnull, 'w')
+# the script will test all the files starting with a specified test_prefix
+test_prefix = "t"
+
+# if the cmd does not finish in timeout seconds, the test result will be considered as a hang
+timeout = 20
+
+# if there are hang_num successive hangs, no need to continue the testing on the current cmd
+# check the way you use the cmd, or increase the timeout and retest, or consider the results as hangs
+hang_num = 3
+
+# the script will test each cmd in run.master on the test cases in test_dir
 all_utilities_path = "./run.master"
 
-# test cases path
-test_list = []
-test_list = os.listdir(test_path)
-test_list = [file for file in test_list if file.startswith(test_prefix)]
-test_list = ["%s%s" % (test_path, file) for file in test_list]
+# the result will be saved in output_dir, each cmd corresponds to a result file 
+output_dir = "./output_log_899"
 
+# the script will combine result files into single file named combine_filename
+combine_filename = "all"
+############
+
+
+# redirect the output to /dev/null, otherwise the shell will be overwhelmed by outputs
+fnull = open(os.devnull, 'w')
+
+
+# get path of all test cases
+test_list = []
+test_list = os.listdir(test_dir)
+test_list = [file for file in test_list if file.startswith(test_prefix)]
+test_list = ["%s%s" % (test_dir, file) for file in test_list]
 
 def run_file(item, output, test_list, fnull, timeout):
   type = item.split(" ", 1)[0]
@@ -27,8 +45,7 @@ def run_file(item, output, test_list, fnull, timeout):
   hang_count = 0
   retcode = 0
   for test_case in test_list:
-    #print(test_case)
-    if hang_count >= arg_error_detect:
+    if hang_count >= hang_num:
       break
     try:
       retcode = subprocess.call("%s %s" % (cmd, test_case), shell=True, stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
@@ -52,7 +69,7 @@ def run_cp(item, output, test_list, fnull, timeout):
   retcode = 0
   for test_case in test_list:
     #print(test_case)
-    if hang_count >= arg_error_detect:
+    if hang_count >= hang_num:
       break
     try:
       subprocess.call("cp %s %s" % (test_case, file_tmp), shell=True)
@@ -77,7 +94,7 @@ def run_stdin(item, output, test_list, fnull, timeout):
   retcode = 0
   for test_case in test_list:
     #print(test_case)
-    if hang_count >= arg_error_detect:
+    if hang_count >= hang_num:
       break
     try:
       retcode = subprocess.call("%s < %s" % (cmd, test_case), shell=True, stdout=fnull, stderr=subprocess.STDOUT, timeout=timeout)
@@ -99,7 +116,7 @@ def run_double(item, output, test_list, fnull, timeout):
   hang_count = 0
   retcode = 0
   for i in range(len(test_list)):
-    if hang_count >= arg_error_detect:
+    if hang_count >= hang_num:
       break
     test_case1 = random.choice(test_list)
     test_case2 = random.choice(test_list)
@@ -125,8 +142,8 @@ def run_double(item, output, test_list, fnull, timeout):
 
 # start
 # create dir for log
-if not os.path.exists(output_filename_dir):
-  os.mkdir(output_filename_dir)
+if not os.path.exists(output_dir):
+  os.mkdir(output_dir)
 
 # open run.master and test every cmd
 with open(all_utilities_path, "r") as all_utilities_file:
@@ -139,7 +156,7 @@ with open(all_utilities_path, "r") as all_utilities_file:
     if type == "run.stdin":
       cmd = item.split(" ", 1)[1]
       #print("h1")
-      file_name = os.path.join(output_filename_dir, "%s.%s" % (type, cmd.replace("/", "-")))
+      file_name = os.path.join(output_dir, "%s.%s" % (type, cmd.replace("/", "-")))
       if os.path.exists(file_name) and os.stat(file_name).st_size != 0:
         with open(file_name, "r") as f:
           if f.readlines()[-1] == "finished\n":
@@ -153,7 +170,7 @@ with open(all_utilities_path, "r") as all_utilities_file:
 
     elif type == "run.file":
       cmd = item.split(" ", 1)[1]
-      file_name = os.path.join(output_filename_dir, "%s.%s" % (type, cmd.replace("/", "-")))
+      file_name = os.path.join(output_dir, "%s.%s" % (type, cmd.replace("/", "-")))
       if os.path.exists(file_name) and os.stat(file_name).st_size != 0:
         with open(file_name, "r") as f:
           if f.readlines()[-1] == "finished\n":
@@ -166,7 +183,7 @@ with open(all_utilities_path, "r") as all_utilities_file:
 
     elif type == "run.cp":
       cmd = item.split(" ", 2)[2]
-      file_name = os.path.join(output_filename_dir, "%s.%s" % (type, cmd.replace("/", "-")))
+      file_name = os.path.join(output_dir, "%s.%s" % (type, cmd.replace("/", "-")))
       if os.path.exists(file_name) and os.stat(file_name).st_size != 0:
         with open(file_name, "r") as f:
           if f.readlines()[-1] == "finished\n":
@@ -179,7 +196,7 @@ with open(all_utilities_path, "r") as all_utilities_file:
 
     elif type == "run.double":
       cmd = item.split(" ", 1)[1]
-      file_name = os.path.join(output_filename_dir, "%s.%s" % (type, cmd.replace("/", "-")))
+      file_name = os.path.join(output_dir, "%s.%s" % (type, cmd.replace("/", "-")))
       if os.path.exists(file_name) and os.stat(file_name).st_size != 0:
         with open(file_name, "r") as f:
           if f.readlines()[-1] == "finished\n":
@@ -194,7 +211,13 @@ with open(all_utilities_path, "r") as all_utilities_file:
   fnull.close()
 
 
-
+# combine the result files
+with open(combine_filename, "w") as file_to_write:
+  dir = os.listdir(output_dir)
+  dir.sort()
+  for item in dir:
+    with open(os.path.join(source_dir, item), "r") as file_to_read:
+      file_to_write.write(file_to_read.read())
 
 
 
